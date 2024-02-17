851787acb6e8e265776b29a4af4bf1d5221446a2
miguel_geniza <nvg25@drexel.edu>
2024-02-16 22:43:49 -0500

    Game.hpp and main.cpp added

---
+++ GameEngine/GameEngine/Game.hpp
@@ -0,0 +1,26 @@
+#ifndef Game_hpp
+#define Game_hpp
+#include "SDL.h"
+#include <stdio.h>
+
+class Game {
+public:
+	Game();
+	~Game();
+
+	void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+
+	bool running();
+
+private:
+	bool isRunning;
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+};
+
+#endif /* Game_hpp */
