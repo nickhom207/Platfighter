c43e4e53b8f9d9298c332bea150c022ea1283556
miguel_geniza <nvg25@drexel.edu>
2024-03-10 21:31:38 -0400

    added audio fixes and new way of using audio

--- GameEngine/GameEngine/Src/Audio.hpp
+++ GameEngine/GameEngine/Src/Audio.hpp
@@ -1,13 +1,21 @@
 #ifndef Audio_hpp
 #define Audio_hpp
 #include "SDL.h"
+#include "SDL_mixer.h"
 
 class Audio {
 public:
     ~Audio();
-    void load(const char* filename);
-    void play();
+    int loadSound(const char* filename);
+    int loadMusic(const char* filename);
+    int playSound(int s, int pan);
+    int playMusic(int s);
+    void stop();
     void setVolume(int volume);
+    void setPan(int channel, int pan);
+
+    int INIT_Mixer();
+    void QUIT_Mixer();
 
 private:
     SDL_AudioSpec wavSpec;
@@ -15,6 +23,7 @@ private:
     Uint8* wavBuffer;
     SDL_AudioDeviceID deviceId;
     int volume = 30;
+    double pan = 0.0;
 };
 
 #endif /* Audio_hpp */
